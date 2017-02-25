09a8cc717092df9781d5ddc1d3a4a8a11a554408
aritra bhawani <eman.arghya@gmail.com>
2017-02-25 17:19:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne ={
+    title: 'article one | aritra bhawani',
+    heading:'article one',
+    content:`<p>
+                this is the content of this web page, whis is my first article
+            </p>`
+};
+function createTemplate(data){
+ var title = data.title;
+ var heading = data.heading;
+ var content=data.cotent;
+ var htmlTemplate=`
+    <!doctype html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name=viewport content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h2>
+                ${heading}
+            </h2>
+            <div>
+                ${content}
+            </div>
+        </div>
+            
+    </body>
+    </html>
+    `;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req,res){
