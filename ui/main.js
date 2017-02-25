a10a57c371c9dbaab9d9a7d4e37a5f50e720ca31
aritra bhawani <eman.arghya@gmail.com>
2017-02-25 18:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
 console.log('Loaded!');
+
 var img = document.getElementById('madi');
+
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style.marginLegft=marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,100);
