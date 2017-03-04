999a254b657a1baba803e4737c3d84046a6b9caa
aritra bhawani <eman.arghya@gmail.com>
2017-03-05 01:13:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,100);
-    //img.style.marginLeft='100px';
+//counter
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    //craeate a request object
+    var request = new XMLHttpRequest();
+    //capture the respons and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                
+            }
+        }
+        //not done yet
+        
+    };
+    //make a requwest
+    request.open('GET' , 'http://http://aritra-bhawani.imad.hasura-app.io/counter' , true);
+    request.send(null);
 };
